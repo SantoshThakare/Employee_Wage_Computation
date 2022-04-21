4302fda3588d39bcb1b518e7da284e690db56f84
SantoshThakare <santoshthakare50@gmail.com>
2022-04-21 22:41:42 +0530

    Calculating daily EmpWage with PartTime Added

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,14 +1,21 @@
 import random
-IS_PRESENT = 1
+
+IS_PRESENT = 2
+IS_PART_TIME = 1
+IS_ABSENT = 0
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
+PART_TIME_HOURS = 4
 
 def get_work_hours():
 
-    emp_check = random.randint(0,1)
+    work_hrs = 0
+    emp_check = random.randint(0,2)
+
     if emp_check == IS_PRESENT:
         work_hrs = FULL_DAY_HOUR
-
+    elif emp_check == IS_PART_TIME:
+        work_hrs == PART_TIME_HOURS
     else:
         work_hrs = 0
     return work_hrs
