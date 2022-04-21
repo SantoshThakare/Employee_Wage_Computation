135bbfe592de89bf5be7893e113793e1c81e6ae5
SantoshThakare <santoshthakare50@gmail.com>
2022-04-21 22:18:17 +0530

    Calculate Daily Wage

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,8 +1,18 @@
 import random
 IS_PRESENT = 1
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
 
-emp_check = random.randint(0,1)
-if emp_check == IS_PRESENT:
-    print("Employee is present")
-else:
-    print("Employee is absent")
+def get_work_hours():
+
+    emp_check = random.randint(0,1)
+    if emp_check == IS_PRESENT:
+        work_hrs = FULL_DAY_HOUR
+
+    else:
+        work_hrs = 0
+    return work_hrs
+
+daily_emp_wage = WAGE_PER_HOUR * get_work_hours()
+print(f"Person Earn  {daily_emp_wage} Rupees this day")
+
