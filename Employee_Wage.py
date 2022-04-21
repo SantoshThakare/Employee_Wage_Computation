a82203128f8ef8891252b84f290b87560e05b196
SantoshThakare <santoshthakare50@gmail.com>
2022-04-21 23:28:46 +0530

    Using Match Case Added

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,25 +1,31 @@
 import random
 
-IS_PRESENT = 2
-IS_PART_TIME = 1
-IS_ABSENT = 0
-WAGE_PER_HOUR = 20
-FULL_DAY_HOUR = 8
-PART_TIME_HOURS = 4
+
+class Constants:
+    IS_PRESENT = 2
+    IS_PART_TIME = 1
+    IS_ABSENT = 0
+    WAGE_PER_HOUR = 20
+    PART_TIME_HOURS = 4
+    FULL_DAY_HOURS = 8
+    MAX_WORKING_DAYS_PER_MONTH = 20
 
 def get_work_hours():
 
     work_hrs = 0
     emp_check = random.randint(0,2)
 
-    if emp_check == IS_PRESENT:
-        work_hrs = FULL_DAY_HOUR
-    elif emp_check == IS_PART_TIME:
-        work_hrs == PART_TIME_HOURS
-    else:
-        work_hrs = 0
+    match emp_check:
+        case Constants.IS_PRESENT:
+            work_hrs = Constants.FULL_DAY_HOURS
+        case Constants.IS_PART_TIME:
+            work_hrs = Constants.PART_TIME_HOURS
+        case Constants.IS_ABSENT:
+            work_hrs = 0
     return work_hrs
 
-daily_emp_wage = WAGE_PER_HOUR * get_work_hours()
-print(f"Person Earn  {daily_emp_wage} Rupees this day")
+monthly_emp_wage = 0
+for i in range(1,Constants.MAX_WORKING_DAYS_PER_MONTH + 1):
+    monthly_emp_wage += Constants.WAGE_PER_HOUR * get_work_hours()
 
+print(f"Person earns {monthly_emp_wage} rupees this month")
