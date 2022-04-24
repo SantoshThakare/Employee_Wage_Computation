eccb6287af55ae73346836a032bba7d200a1882f
SantoshThakare <santoshthakare50@gmail.com>
2022-04-24 09:06:20 +0530

    Calculating EmpWage till maxWorkHrs or maxWorkDays

--- Employee_Wage.py
+++ Employee_Wage.py
@@ -1,6 +1,6 @@
 import random
 
-
+#Constants Class to get Constants for the Programs
 class Constants:
     IS_PRESENT = 2
     IS_PART_TIME = 1
@@ -9,9 +9,15 @@ class Constants:
     PART_TIME_HOURS = 4
     FULL_DAY_HOURS = 8
     MAX_WORKING_DAYS_PER_MONTH = 20
+    MAX_WORK_HRS_PER_MONTH = 100
 
 def get_work_hours():
-
+    '''
+            Description: Function to get Employee Working hours
+            Parameter: None
+            Return: work_hrs as per Random Calculation
+        '''
+    #Variables
     work_hrs = 0
     emp_check = random.randint(0,2)
 
@@ -24,8 +30,12 @@ def get_work_hours():
             work_hrs = 0
     return work_hrs
 
-monthly_emp_wage = 0
-for i in range(1,Constants.MAX_WORKING_DAYS_PER_MONTH + 1):
-    monthly_emp_wage += Constants.WAGE_PER_HOUR * get_work_hours()
+#Initializing Variables with zero value
+monthly_emp_wage = total_work_hrs = total_work_days = 0
+while total_work_hrs <= Constants.MAX_WORK_HRS_PER_MONTH and total_work_days < Constants.MAX_WORKING_DAYS_PER_MONTH:
+    work_hrs_per_day = get_work_hours()
+    total_work_hrs += work_hrs_per_day
+    total_work_days += 1
 
+monthly_emp_wage = Constants.WAGE_PER_HOUR * total_work_hrs
 print(f"Person earns {monthly_emp_wage} rupees this month")
